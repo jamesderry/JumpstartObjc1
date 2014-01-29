3c54442a69cd637550e8ef3cd79b0611b51e2918
James Derry <jderry@derryitconsulting.com>
2014-01-28 20:21:05 -0600

    Initial Commit

---
+++ JumpstartObjc1a/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  JumpstartObjc1a
+//
+//  Created by James Derry on 1/28/14.
+//  Copyright (c) 2014 James Derry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
